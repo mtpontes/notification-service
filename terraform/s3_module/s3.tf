7d61377a9f88527989ac72ecc7d8291f64dd9cbf
Mateus Pontes <mateuspsdd@gmail.com>
2025-05-09 23:23:00 -0300

    chore: fix terraform

--- terraform/s3_module/s3.tf
+++ terraform/s3_module/s3.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "notification_service_source_bucket" {
 resource "aws_s3_object" "publisher_lambda_zip_object" {
   bucket = aws_s3_bucket.notification_service_source_bucket.id
   key    = "${var.publisher_service_lambda_file_zip_name}.zip"
-  source = var.publisher_service_lambda_file_zip_name
+  source = "${var.publisher_service_lambda_file_zip_name}.zip"
   etag = filemd5("${var.publisher_service_lambda_file_zip_name}.zip")
   content_type = "application/zip"
 }
@@ -17,7 +17,7 @@ resource "aws_s3_object" "publisher_lambda_zip_object" {
 resource "aws_s3_object" "dispatcher_lambda_zip_object" {
   bucket = aws_s3_bucket.notification_service_source_bucket.id
   key    = "${var.dispatcher_service_lambda_file_zip_name}.zip"
-  source = var.dispatcher_service_lambda_file_zip_name
+  source = "${var.dispatcher_service_lambda_file_zip_name}.zip"
   etag = filemd5("${var.dispatcher_service_lambda_file_zip_name}.zip")
   content_type = "application/zip"
 }
