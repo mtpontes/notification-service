e37d35aaabffea717d81fdf4f63b97edd2b915a5
Mateus Pontes <mateuspsdd@gmail.com>
2025-05-09 23:35:10 -0300

    fix: terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,9 +7,11 @@ variable "region" { # TF_VAR_REGION
 
 variable "publisher_service_lambda_file_zip_name" { # TF_VAR_PUBLISHER_SERVICE_LAMBDA_FILE_ZIP_NAME
   type = string
+  default = "publisher_service"
 }
 variable "dispatcher_service_lambda_file_zip_name" { # TF_VAR_DISPATCHER_SERVICE_LAMBDA_FILE_ZIP_NAME
   type = string
+  default = "dispatcher_service"
 }
 
 variable "whatsapp_api_token" { # TF_VAR_WHATSAPP_API_TOKEN
