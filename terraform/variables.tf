a878ce4dc90e6ae79d64af98f41f68fbb9b1bfc6
Mateus Pontes <mateuspsdd@gmail.com>
2025-05-10 16:12:27 -0300

    chore: fix terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,7 +12,6 @@ variable "publisher_source_code_lambda_s3_zip_name" { # TF_VAR_PUBLISHER_SERVICE
 }
 variable "publisher_source_code_zip" { # workflows -> build -> build-artifacts -> strategy -> matrix -> service
   type = string
-  default = "notification-publisher"
 }
 
 # variable "dispatcher_service_lambda_file_zip_name" { # TF_VAR_DISPATCHER_SERVICE_LAMBDA_FILE_ZIP_NAME
@@ -22,7 +21,6 @@ variable "dispatcher_source_code_lambda_s3_zip_name" { # TF_VAR_DISPATCHER_SERVI
 }
 variable "dispatcher_source_code_zip" { # workflows -> build -> build-artifacts -> strategy -> matrix -> service
   type = string
-  default = "notification-dispatcher"
 }
 
 variable "whatsapp_api_token" { # TF_VAR_WHATSAPP_API_TOKEN
