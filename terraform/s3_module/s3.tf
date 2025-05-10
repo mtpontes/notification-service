052cd49800f6578e0316480e076af72ef659bd0a
Mateus Pontes <mateuspsdd@gmail.com>
2025-05-09 23:26:19 -0300

    fix: terraform

--- terraform/s3_module/s3.tf
+++ terraform/s3_module/s3.tf
@@ -7,17 +7,18 @@ resource "aws_s3_bucket" "notification_service_source_bucket" {
 }
 
 resource "aws_s3_object" "publisher_lambda_zip_object" {
-  bucket = aws_s3_bucket.notification_service_source_bucket.id
-  key    = "${var.publisher_service_lambda_file_zip_name}.zip"
-  source = "${var.publisher_service_lambda_file_zip_name}.zip"
-  etag = filemd5("${var.publisher_service_lambda_file_zip_name}.zip")
+  bucket       = aws_s3_bucket.notification_service_source_bucket.id
+  key          = "${var.publisher_service_lambda_file_zip_name}.zip"
+  source       = "${path.module}/${var.publisher_service_lambda_file_zip_name}.zip"
+  etag         = filemd5("${path.module}/${var.publisher_service_lambda_file_zip_name}.zip")
   content_type = "application/zip"
 }
 
 resource "aws_s3_object" "dispatcher_lambda_zip_object" {
-  bucket = aws_s3_bucket.notification_service_source_bucket.id
-  key    = "${var.dispatcher_service_lambda_file_zip_name}.zip"
-  source = "${var.dispatcher_service_lambda_file_zip_name}.zip"
-  etag = filemd5("${var.dispatcher_service_lambda_file_zip_name}.zip")
+  bucket       = aws_s3_bucket.notification_service_source_bucket.id
+  key          = "${var.dispatcher_service_lambda_file_zip_name}.zip"
+  source       = "${path.module}/${var.dispatcher_service_lambda_file_zip_name}.zip"
+  etag         = filemd5("${path.module}/${var.dispatcher_service_lambda_file_zip_name}.zip")
   content_type = "application/zip"
 }
+
