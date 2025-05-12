5af6e9df571353a8303779bfb85bffc9a41b1ff3
Mateus Pontes <mateuspsdd@gmail.com>
2025-05-11 23:09:58 -0300

    chore: Uptimizing query by events

--- notification-publisher/src/main/infra/db/repositories/mongodb_event_repository_impl.py
+++ notification-publisher/src/main/infra/db/repositories/mongodb_event_repository_impl.py
@@ -1,4 +1,4 @@
-from bson import ObjectId
+from datetime import datetime
 
 from src.main.infra.utils.log_utils import log
 from src.main.domain.models.event_model import EventModel
@@ -8,7 +8,7 @@ from src.main.domain.repositories.event_repository_i import EventRepositoryI
 class MongodbEventRepositoryImpl(EventRepositoryI):
 
     def find_all_by_user(self, user_id: str) -> list[EventModel]:
-        events: list[EventModel] = list(EventModel.objects(user=user_id)) # pylint: disable=no-member
+        events: list[EventModel] = list(EventModel.objects(user=user_id, dt_end__lt=datetime.now())) # pylint: disable=no-member
         log.info('%s - [find_all_by_user] - All events: %s', self.__class__.__name__, len(events))
 
         return events
