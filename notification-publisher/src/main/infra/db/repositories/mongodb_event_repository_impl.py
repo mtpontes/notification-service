8ef944dfdf0b8cdc220fc8accce8bd4b2bf59982
Mateus Pontes <mateuspsdd@gmail.com>
2025-05-14 19:22:33 -0300

    bug-fix: fixing query in MongoDB

--- notification-publisher/src/main/infra/db/repositories/mongodb_event_repository_impl.py
+++ notification-publisher/src/main/infra/db/repositories/mongodb_event_repository_impl.py
@@ -8,7 +8,7 @@ from src.main.domain.repositories.event_repository_i import EventRepositoryI
 class MongodbEventRepositoryImpl(EventRepositoryI):
 
     def find_all_by_user(self, user_id: str) -> list[EventModel]:
-        events: list[EventModel] = list(EventModel.objects(user=user_id, dt_end__lt=datetime.now())) # pylint: disable=no-member
+        events: list[EventModel] = list(EventModel.objects(user=user_id, dt_end__gte=datetime.now())) # pylint: disable=no-member
         log.info('%s - [find_all_by_user] - All events: %s', self.__class__.__name__, len(events))
 
         return events
